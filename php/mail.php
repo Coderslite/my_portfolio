bf6053336b4f9411d8b90ed13f9c651bba85ed13
Nabila Momand <nabilamomand137@gmail.com>
2022-06-14 02:32:25 +0100

    some changes

--- php/mail.php
+++ php/mail.php
@@ -1,20 +1,19 @@
 <?php
-// require 'composer/vendor/autoload.php';
-// use PHPMailer\PHPMailer\PHPMailer;
+require 'composer/vendor/autoload.php';
+use PHPMailer\PHPMailer\PHPMailer;
 
 
-// $mail = new PHPMailer();
-// $mail->isSMTP();
-// $mail->Host = 'stmp.gmail.com';
-// $mail->SMTPAuth = true;
-// $mail->Username = 'abrahamgreatebele@gmail.com'; // your mail address
-// $mail->Password = 'Mesomorph'; // your mail passwor
-// $mail->addReplyTo('abrahamgreatebele@gmail.com', 'Your Name');
-// $mail->addAddress('abrahamgreatebele@gmail.com', 'Great');
-// $mail->SMTPSecure = 'ssl';
-// $mail->Port =  465;
-// // Sender info 
-// $mail->setFrom('abrahamgreatebele@gmail.com', 'Coderslite');
+$mail = new PHPMailer();
+// configure an SMTP
+$mail->isSMTP();
+$mail->Host = 'localhost';
+$mail->SMTPAuth = false;
+$mail->SMTPAutoTLS = false; 
+$mail->addReplyTo('abrahamgreatebele@gmail.com', 'Your Name');
+$mail->addAddress('abrahamgreatebele@gmail.com', 'Great');
+$mail->Port =  25;
+// Sender info 
+$mail->setFrom('abrahamgreatebele@gmail.com', 'Coderslite');
 
 
 // $name = $_POST['name'];
@@ -23,31 +22,31 @@
 // $message = $_POST['message'];
 
 
-// // Add a recipient 
-// // $mail->addAddress('abrahamgreatebele@gmail.com');
-// // Set email format to HTML 
-// $mail->isHTML(true); 
+// Add a recipient 
+// $mail->addAddress('abrahamgreatebele@gmail.com');
+// Set email format to HTML 
+$mail->isHTML(true); 
 
-// // Mail subject 
-// $mail->Subject = 'Login Successful';
+// Mail subject 
+$mail->Subject = 'Login Successful';
 
-// // Mail body content 
-// $bodyContent = 
-// '<html>
-// <head></head>
-// <body>
-// <h1>Login Detection</h1>
-// <h2>we detected a login activity on your dashboard</h2>
-// <h2>If not you, login and change your password immediately</h2>
-// <body>
-// </html>';
-// // $bodyContent .= $template; 
-// $mail->Body = $bodyContent;
-// if (!$mail->send()) {
-//     $data = 'failed';
-// } else {
-//     $data = 'success';
-// }
+// Mail body content 
+$bodyContent = 
+'<html>
+<head></head>
+<body>
+<h1>Login Detection</h1>
+<h2>we detected a login activity on your dashboard</h2>
+<h2>If not you, login and change your password immediately</h2>
+<body>
+</html>';
+// $bodyContent .= $template; 
+$mail->Body = $bodyContent;
+if (!$mail->send()) {
+    $data = 'failed';
+} else {
+    $data = 'success';
+}
 $data = "success";
 header('content-Type: application/json');
 echo json_encode($data);
